41e0c6545e0d6a95edec54db7b72bd8271c3841c
Wulfcastle <reiner.wulfcastle@gmail.com>
2015-08-08 00:11:51 +0200

    P1 - November 2011 : Question 2 Complete

--- P1 - November 2011 [ ]/Questions/Question 2/Question2/src/Household.java
+++ P1 - November 2011 [ ]/Questions/Question 2/Question2/src/Household.java
@@ -1,34 +1,87 @@
-   public class Household
-   {   
-      private String account;
-      private int members;
-   	private int [] arrWaterUse;
-              
-      public Household()
-      {
-      
-      }
-   
-      public Household(String Account, int Members, int[]arrWater)
-      {
-  
-  		}
-   
-          
-      public boolean determineHighRisk(double dayLimit)
-      {
-                
-         return false;
-      			 
-      }
+
+public class Household {
+
+    private String account;
+    private int members;
+    private int[] arrWaterUse;
+
+    public Household() {
+
+    }
+
+    public Household(String Account, int Members, int[] arrWater) {
+        this.account = Account;
+        this.members = Members;
+        this.arrWaterUse = arrWater;
+
+    }
+
+    public int calculateTotal() {
+        int total = 0;
+        for (int usage : arrWaterUse) {
+            total += usage;
+        }
+        return total;
+    }
+
+    public double calculateAvg() {
+        double total = this.calculateTotal();
+        double average = total / 7;
+        return average;
+    }
     
-         
-                    
-      public String toString()
-      {
-         String objStr = "Account number: " + account  + "Number of members: " + members;
-         return objStr;
-      }
+    public int determineHighDay() {
+        double highestUsage = 0.0;
+        int day = 0;
+        for (int i=0; i < arrWaterUse.length; i++) {
+            if (arrWaterUse[i] > highestUsage) {
+                highestUsage = arrWaterUse[i];
+                day = i;
+            }
+        }
+        return day + 1;
+    }
+
+    public boolean determineHighRisk(double dayLimit) {
+        boolean highRisk = false;
+        double average = this.calculateAvg();
         
+        int numberExceeded = 0;
+        for (int dailyUsage : arrWaterUse) {
+            if (dailyUsage > dayLimit) {
+                numberExceeded++;
+            }
+        }
         
-   }
+        if (average < dayLimit) {
+            if (numberExceeded <= 2) {
+                highRisk = false;
+            }
+        } else { 
+            highRisk = true;
+        }
+        return highRisk;
+    }
+    
+    public int[] getWaterUsage() {
+        return this.arrWaterUse;
+    }
+
+    public String toString() {
+        String accountDetails = String.format("Account number: %s\n"
+                + "Number of members: %d\n"
+                + "Daily Water Usage :\n", account, members);
+        String days = "Days:       ";
+        for (int i=1; i < 8; i++) {
+            days += String.format("%10d", i);
+        }
+        String usageDetails = "Water Used:";
+        for (int usage : arrWaterUse) {
+            usageDetails += String.format("%10d", usage);
+        }
+        
+        String output = String.format("\n\n%s\n%s\n%s\n\n", accountDetails, days, usageDetails);
+        return output;
+    }
+
+}
--- P1 - November 2011 [ ]/Questions/Question 2/Question2/src/TestQuestion2.java
+++ P1 - November 2011 [ ]/Questions/Question 2/Question2/src/TestQuestion2.java
@@ -1,59 +1,92 @@
+
 import java.util.Scanner;
 
-public class TestQuestion2
-{
-	
-	public static void main(String args[]) throws Exception
-	{	
-	    String accountNumber = "AC-23245";
-	    int members = 4;    
-	    int [] arrWaterUse =  {481, 128, 434, 253, 331, 366, 132};
-				
-		Scanner input = new Scanner(System.in);
-		char ch = ' ';
-		while (ch != 'Q')
-		{
-			System.out.println();
-			System.out.println("         Menu");
-			System.out.println(" ");
-			System.out.println("       Option A "); 
-			System.out.println("       Option B "); 
-			System.out.println("       Option C "); 
-			System.out.println(" ");
-			System.out.println("       Q - QUIT");
-			System.out.println(" ");
-			System.out.print("       Your choice? ");
-		
-			ch = input.nextLine().toUpperCase().charAt(0);
-		
-			switch (ch)
-			{
-				case 'A':
-				{
-				
-				break; 
-				}
-			
-				case 'B':
-				{
-				         		 
-				  	break; 
-				} 
- 
-				case 'C':
-				{
-				  
-				   break; 
-				} 
-				case 'Q':
-				{
-					System.exit(0);
-				} // case
-				
-			} // switch
-			
-		} // while
-		
-	} // main
-	
+public class TestQuestion2 {
+
+    public static void main(String args[]) throws Exception {
+
+        String accountNumber = "AC-23245";
+
+        int members = 4;
+
+        int[] arrWaterUse = {481, 438, 454, 353, 421, 396, 432};
+
+        Household household = new Household(accountNumber, members, arrWaterUse);
+
+        Scanner input = new Scanner(System.in);
+        char ch = ' ';
+        while (ch != 'Q') {
+            System.out.println();
+            System.out.println("         Menu");
+            System.out.println(" ");
+            System.out.println("       Option A ");
+            System.out.println("       Option B ");
+            System.out.println("       Option C ");
+            System.out.println(" ");
+            System.out.println("       Q - QUIT");
+            System.out.println(" ");
+            System.out.print("       Your choice? ");
+
+            ch = input.nextLine().toUpperCase().charAt(0);
+
+            switch (ch) {
+                case 'A': {
+                    String amountDetails = String.format("Total water usage: %d litres\n"
+                            + "Average water usage: %.1f litres", household.calculateTotal(), household.calculateAvg());
+                    System.out.print(String.format("%s\n%s\n\n", household.toString(), amountDetails));
+                    break;
+                }
+
+                case 'B': {
+
+                    String headings = String.format("\n\nDays & Amount of Water Exceeding the Average"
+                            + "\n============================================\n"
+                            + "Average Water Usage (per day) : %.2f litres\n\n"
+                            + "%s %25s\n", household.calculateAvg(), "Days", "Value exceeding average (in litres)");
+
+                    int[] usageDetails = household.getWaterUsage();
+
+                    String details = "";
+                    for (int i = 0; i < usageDetails.length; i++) {
+                        double average = household.calculateAvg();
+                        double difference = usageDetails[i] - average;
+                        if (difference > 0) {
+                            details += String.format("%d %25.2f\n", i + 1, difference);
+                        }
+                    }
+
+                    String output = headings + details;
+                    System.out.print(output + "\n\n");
+
+                    break;
+                }
+
+                case 'C': {
+                    String risk = "";
+                    
+                    if (household.determineHighRisk(400.00) == false) {
+                        risk = "Low-Risk Household";
+                    } else {
+                        risk = "High-Risk Household";
+                    }
+                    
+                    int highestUsageDay = household.determineHighDay();
+                    
+                    String output = String.format("%s\n\n"
+                            + "The day on which the most water was used: %d\n"
+                            + "%s\n", household.toString(), highestUsageDay, risk);
+                    System.out.print(output);
+
+                    break;
+                }
+                case 'Q': {
+                    System.exit(0);
+                } // case
+
+            } // switch
+
+        } // while
+
+    } // main
+
 } // class
